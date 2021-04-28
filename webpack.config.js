b119e93884ba8537bf8c8297451e180448369ed6
sramdoss <sharath_ramdoss@intuit.com>
2021-04-28 09:36:13 +0530

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,120 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+const TerserWebpackPlugin = require("terser-webpack-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
+module.exports = function (_env, argv) {
+    const isProduction = argv.mode === "production";
+    const isDevelopment = !isProduction;
+
+    return {
+        devtool: isDevelopment && "cheap-module-source-map",
+        entry: "./src/index.js",
+        output: {
+            path: path.resolve(__dirname, "dist"),
+            filename: "assets/js/[name].[contenthash:8].js",
+            publicPath: "/"
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.jsx?$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: "babel-loader",
+                        options: {
+                            cacheDirectory: true,
+                            cacheCompression: false,
+                            envName: isProduction ? "production" : "development"
+                        }
+                    }
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        isProduction ? MiniCssExtractPlugin.loader : "style-loader",
+                        "css-loader"
+                    ]
+                },
+                {
+                  test: /\.(png|svg|jpg|jpeg|gif)$/,
+                  use: ['file-loader']
+                }
+                // {
+                //     test: /\.svg$/,
+                //     use: ["@svgr/webpack"]
+                // }
+            ]
+        },
+        resolve: {
+            extensions: [".js", ".jsx"]
+        },
+        plugins: [
+            isProduction &&
+            new MiniCssExtractPlugin({
+                filename: "assets/css/[name].[contenthash:8].css",
+                chunkFilename: "assets/css/[name].[contenthash:8].chunk.css"
+            }),
+            new HtmlWebpackPlugin({
+                template: path.resolve(__dirname, "public/index.html"),
+                inject: true
+            }),
+            new webpack.DefinePlugin({
+                "process.env.NODE_ENV": JSON.stringify(
+                    isProduction ? "production" : "development"
+                )
+            })
+        ].filter(Boolean),
+        optimization: {
+            minimize: isProduction,
+            minimizer: [
+                new TerserWebpackPlugin({
+                    terserOptions: {
+                        compress: {
+                            comparisons: false
+                        },
+                        mangle: {
+                            safari10: true
+                        },
+                        output: {
+                            comments: false,
+                            ascii_only: true
+                        },
+                        warnings: false
+                    }
+                }),
+                new OptimizeCssAssetsPlugin()
+            ],
+            splitChunks: {
+                chunks: "all",
+                minSize: 0,
+                maxInitialRequests: 10,
+                maxAsyncRequests: 10,
+                cacheGroups: {
+                    vendors: {
+                        test: /[\\/]node_modules[\\/]/,
+                        name(module, chunks, cacheGroupKey) {
+                            const packageName = module.context.match(
+                                /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+                            )[1];
+                            return `${cacheGroupKey}.${packageName.replace("@", "")}`;
+                        }
+                    },
+                    common: {
+                        minChunks: 2,
+                        priority: -10
+                    }
+                }
+            },
+            runtimeChunk: "single"
+        },
+        devServer: {
+            compress: true,
+            historyApiFallback: true,
+            open: true,
+            overlay: true
+        }
+    };
+};
